1b6dec1a3663580c9ffe79e88594a8ac0bd3bf2b
BookStore <ivansmgo@gmail.com>
2021-01-31 09:11:30 +0100

    Updates

---
+++ precache-manifest.9a804942ba704408636f51e2e19b86ba.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3021c3926f8098496216fe056639a470",
+    "url": "/tmparab/index.html"
+  },
+  {
+    "revision": "462da7b7bc2d8272670f",
+    "url": "/tmparab/static/css/2.e8266809.chunk.css"
+  },
+  {
+    "revision": "3ffce93eac373f300e7c",
+    "url": "/tmparab/static/css/main.37920d2d.chunk.css"
+  },
+  {
+    "revision": "462da7b7bc2d8272670f",
+    "url": "/tmparab/static/js/2.9c6c642f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/tmparab/static/js/2.9c6c642f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ffce93eac373f300e7c",
+    "url": "/tmparab/static/js/main.3b69ddb8.chunk.js"
+  },
+  {
+    "revision": "1b75d56aaae4ef347cd6",
+    "url": "/tmparab/static/js/runtime-main.3649585c.js"
+  },
+  {
+    "revision": "d4a462442ee5d2bf128135d834a4942e",
+    "url": "/tmparab/static/media/arab2.d4a46244.jpg"
+  },
+  {
+    "revision": "e45354cad7cf5b9bbb858d8f0fb26adb",
+    "url": "/tmparab/static/media/bathroom.e45354ca.webp"
+  },
+  {
+    "revision": "f856b33f2721a429462405bafbeee04b",
+    "url": "/tmparab/static/media/bgImg1u.f856b33f.jpg"
+  },
+  {
+    "revision": "e27131991baf36999e30255b442693b5",
+    "url": "/tmparab/static/media/bgImg1w.e2713199.jpg"
+  },
+  {
+    "revision": "cc8f9466e73992ea4b42dd9329084dda",
+    "url": "/tmparab/static/media/bgImg2w.cc8f9466.jpg"
+  },
+  {
+    "revision": "34ca76f592148a898a14e5dab2935050",
+    "url": "/tmparab/static/media/bgImg3w.34ca76f5.jpg"
+  },
+  {
+    "revision": "a840325cbd9654b60c3fef682fcc7493",
+    "url": "/tmparab/static/media/bgImg4w.a840325c.jpg"
+  },
+  {
+    "revision": "fc5e561aad3b0302e0caffcd7b258395",
+    "url": "/tmparab/static/media/couplesRoom.fc5e561a.webp"
+  },
+  {
+    "revision": "7c197385279fdf473dee79083cab82f8",
+    "url": "/tmparab/static/media/cruiser.7c197385.png"
+  },
+  {
+    "revision": "5d06d55a1dc316db88858993e949a837",
+    "url": "/tmparab/static/media/dubai-wave-hotel.5d06d55a.jpg"
+  },
+  {
+    "revision": "ab103ccd5fc5e027be40a5a63cd5b654",
+    "url": "/tmparab/static/media/familyApartment.ab103ccd.webp"
+  },
+  {
+    "revision": "afb6fc649e9da4e97a1e4cd31eceeb75",
+    "url": "/tmparab/static/media/familyEating.afb6fc64.webp"
+  },
+  {
+    "revision": "cf8ab11f90d618731ebeee71ae59fbc0",
+    "url": "/tmparab/static/media/hall.cf8ab11f.webp"
+  },
+  {
+    "revision": "925a4fadd041a12ace06bfd36dcce7cc",
+    "url": "/tmparab/static/media/hotelImg2.925a4fad.jpg"
+  },
+  {
+    "revision": "22dbffbac33783e50c92722ba3e4de79",
+    "url": "/tmparab/static/media/logoDark.22dbffba.png"
+  },
+  {
+    "revision": "87849ad7dad985f40aafb55740fd16e2",
+    "url": "/tmparab/static/media/meetingRoom.87849ad7.jpg"
+  },
+  {
+    "revision": "1b67d8ae18cb185ae0a6865ac6fda1aa",
+    "url": "/tmparab/static/media/museum.1b67d8ae.jpg"
+  },
+  {
+    "revision": "716bdecba3df0a486c4264661c8a620b",
+    "url": "/tmparab/static/media/outside.716bdecb.webp"
+  },
+  {
+    "revision": "3b0a81e0af2bbf60d2743765a2cec540",
+    "url": "/tmparab/static/media/playground.3b0a81e0.webp"
+  },
+  {
+    "revision": "d0231dea373df92429d1d2e9a21f3207",
+    "url": "/tmparab/static/media/pool.d0231dea.webp"
+  },
+  {
+    "revision": "8e480981097c4ccd837d9a0125489abd",
+    "url": "/tmparab/static/media/sauna.8e480981.webp"
+  },
+  {
+    "revision": "e06cdee22ecd55e01383720c4bf6741b",
+    "url": "/tmparab/static/media/singleRoom.e06cdee2.webp"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/tmparab/precache-manifest.9a804942ba704408636f51e2e19b86ba.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tmparab/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
